3d48ec2a0c7060ba3330dc408f2fd7cd6ea89a6d
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-02-10 13:59:14 -0800

    add web page functionality

--- js/title-case-scripts.js
+++ js/title-case-scripts.js
@@ -7,19 +7,15 @@ var capitalize = function(inputWord) {
   return outputWord;
 };
 
-
 var titleCase = function(inputString) {
   var loweredString = inputString.toLowerCase();
   var outputArray = [];
   var words = loweredString.split(" ");
   var shiftedWord = capitalize(words.shift());
-
   if ((words.length) > 0) {
     var poppedWord = capitalize(words.pop());
   }
-
   var forbiddenWords = ["a", "an", "the", "at", "by", "for", "in", "of", "on", "to", "up", "and", "as", "but", "or", "nor"];
-
   words.forEach(function(word) {
     if (forbiddenWords.indexOf(word) === -1) {
       var outputWord = capitalize(word);
@@ -36,7 +32,17 @@ var titleCase = function(inputString) {
     outputArray.push(poppedWord);
     var outputString = outputArray.join(" ");
   }
-
   return outputString;
-
 };
+
+$(document).ready(function() {
+  $("form#title-case").submit(function(event){
+    var stringInput = ($("input#text-input").val());
+    var result = titleCase(stringInput);
+
+    $(".text-output").text(result);
+    $("#result").show();
+    event.preventDefault();
+  });
+
+});
